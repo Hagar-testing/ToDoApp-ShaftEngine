b8f0b90a88d1c6b532b7c144c37c180a2e97b774
Hagar <hagar.testing@gmail.com>
2024-03-28 01:43:09 +0200

    Create ApiResponseConstants

---
+++ src/main/java/com/egyptlaptop/api/ApiResponseConstants.java
@@ -0,0 +1,7 @@
+package com.egyptlaptop.api;
+
+public class ApiResponseConstants {
+    public static final String ACCESS_TOKEN = "access_token";
+    public static final String USER_ID = "userID";
+    public static final String FIRST_NAME = "firstName";
+}
