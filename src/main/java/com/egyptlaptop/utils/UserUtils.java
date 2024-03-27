19918754c2af42aead326d0190e9429d911d852c
Hagar <hagar.testing@gmail.com>
2024-03-28 01:46:19 +0200

    Create UserUtils

---
+++ src/main/java/com/egyptlaptop/utils/UserUtils.java
@@ -0,0 +1,28 @@
+package com.egyptlaptop.utils;
+
+
+import com.github.javafaker.Faker;
+
+import java.util.HashMap;
+
+public class UserUtils {
+
+    public static HashMap<String, String> generateRandomUserHashMap() {
+
+        Faker faker = new Faker();
+
+        // Generate fake user data
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String userEmail = faker.internet().emailAddress();
+        String userPassword = faker.internet().password();
+
+        HashMap<String,String> userHashMap = new HashMap<>();
+        userHashMap.put("firstName", firstName);
+        userHashMap.put("lastName", lastName);
+        userHashMap.put("email", userEmail);
+        userHashMap.put("password", userPassword);
+
+        return userHashMap;
+    }
+}
