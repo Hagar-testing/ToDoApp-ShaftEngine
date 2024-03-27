db131d30244532a3dd38b5b1c210f8ecd92051cb
Hagar <hagar.testing@gmail.com>
2024-03-27 23:55:58 +0200

    Create PagePathsConstant

---
+++ src/main/java/com/egyptlaptop/constants/PagePathsConstant.java
@@ -0,0 +1,7 @@
+package com.egyptlaptop.constants;
+
+public class PagePathsConstant {
+    public static final String TODO_PAGE_PATH = "todo";
+    public static final String NEW_TODO_PAGE_PATH = TODO_PAGE_PATH +"/new";
+
+}
